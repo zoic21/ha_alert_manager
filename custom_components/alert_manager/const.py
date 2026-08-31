9569b1e9b914a39a5c0849c29bd03aa7736f0d94
Loïc <1536036+zoic21@users.noreply.github.com>
2026-08-31 14:12:16 +0200

    Release 2.0.5-dev11

--- custom_components/alert_manager/const.py
+++ custom_components/alert_manager/const.py
@@ -10,7 +10,7 @@ from homeassistant.util.hass_dict import HassKey
 DOMAIN: Final = "alert_manager"
 # This version is also used as the frontend module cache key. It must change
 # whenever the distributed panel bundle changes.
-INTEGRATION_VERSION: Final = "2.0.5-dev10"
+INTEGRATION_VERSION: Final = "2.0.5-dev11"
 FRONTEND_CACHE_VERSION: Final = f"{INTEGRATION_VERSION}.10"
 PLATFORMS: Final = [Platform.BUTTON, Platform.SENSOR, Platform.SWITCH]
 
